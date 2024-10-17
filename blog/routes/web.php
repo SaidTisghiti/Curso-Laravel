7500456b9f7e62c07adfa7b69b338a7faa820a16
SAÏD\saids <stisghitie@insjoaquimmir.cat>
2024-10-17 12:03:55 +0200

    Rutas laraver11

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -3,5 +3,35 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Welcome to the homepage";
 });
+
+Route::get('/posts', function(){
+    return "Aquí se mostrarán todos los posts";
+});
+
+Route::get('/posts/create', function(){
+    return "Aqui se mostrará un formulario para crear un post";
+});
+
+Route::get('/posts/{post}', function($post){
+    return "Aquí es mostrará el post {$post}";
+});
+
+// Route::get('/posts/{post}/{category}', function($post, $category){
+//     return "Aquí es mostrará el post {$post} de la categoria {$category}";
+// });
+
+// Route::get('/posts/{post}/{category?}', function($post, $category = null){
+//     if ($category) {
+//         return "Aquí es mostrará el post {$post} de la categoria {$category}";
+//     }
+//     return "Aquí es mostrará el post {$post}";
+// });
+
+
+//Get
+//Post
+//Put
+//Patch
+//Delete
