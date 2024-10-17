f77f29c09a22848b9510a523ff446712ff94ce69
SAÏD\saids <stisghitie@insjoaquimmir.cat>
2024-10-17 12:26:36 +0200

    Controladores laravel11

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -1,22 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return "Welcome to the homepage";
-});
+Route::get('/', HomeController::class);
 
-Route::get('/posts', function(){
-    return "Aquí se mostrarán todos los posts";
-});
+Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/create', function(){
-    return "Aqui se mostrará un formulario para crear un post";
-});
+Route::get('/posts/create', [PostController::class, 'create']);
 
-Route::get('/posts/{post}', function($post){
-    return "Aquí es mostrará el post {$post}";
-});
+Route::get('/posts/{post}', [PostController::class, 'show']);
 
 // Route::get('/posts/{post}/{category}', function($post, $category){
 //     return "Aquí es mostrará el post {$post} de la categoria {$category}";
