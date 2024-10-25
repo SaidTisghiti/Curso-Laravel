5b794d2dae1c9ae464d66427e195c87e5110cad9
SAÏD\saids <stisghitie@insjoaquimmir.cat>
2024-10-25 10:47:11 +0200

    Relación uno a muchos

--- relationships/routes/web.php
+++ relationships/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Comment;
 use App\Models\Phone;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -13,29 +15,75 @@ Route::get('prueba', function(){
 });
 
 Route::get('prueba', function() {
-    // User::create([
-    //     'name' => 'Victor Arana',
-    //     'email' => 'victor@codersfree.com',
-    //     'password' => bcrypt('12345678')
-    // ]);
+    /*
+    User::create([
+        'name' => 'Victor Arana',
+        'email' => 'victor@codersfree.com',
+        'password' => bcrypt('12345678')
+    ]);
 
-    // return 'Usuario creado';
+    return 'Usuario creado';
+    */
+    
+    /*
+    Phone::create([
+        'number' => '987654321',
+        'user_id' => 1
+    ]);
 
-    // Phone::create([
-    //     'number' => '987654321',
-    //     'user_id' => 1
-    // ]);
+    return 'Telefono creado';
+    */
 
-    // return 'Telefono creado';
+    /*
+    $user = User::where('id', 1)
+                ->with('phone')
+                ->first();
 
-    // $user = User::where('id', 1)
-    //             ->with('phone')
-    //             ->first();
+    return $user;
+    */
 
-    // return $user;
-
+    /*
     $phone = Phone::find(1);
 
     return $phone->user;
+    */
+
+    /*
+    Post::create([
+        'title' => 'Post 2',
+        'content' => 'Contenido del post 2'
+    ]);
+
+    return 'Post creado';
+    */
+    
+    /*
+    Comment::create([
+        'content' => 'Comentario 3',
+        'post_id' => 1
+    ]);
+ 
+    return 'Comentario creado';
+    */
+    
+    /*
+    $post = Post::find(1);
+
+    return $post->comments;
+    */
+    
+    /*
+    $comment = Comment::find(1);
+
+    return $comment->post;
+    */
+    
+    $post = Post::find(2);
+
+    $post->comments()->create([
+        'content' => 'Un comentario de prueba'
+    ]);
+
+    return 'Comentario creado';
 
 });
